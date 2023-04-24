82c75b92f3283a0306de2d19aa8ccc66f2cc6e9b
alv1981 <acasve1981@gmail.com>
2023-04-24 16:52:18 +0200

    test

---
+++ build/CMakeFiles/sonar_scanner_example.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alvaro/repos/linux-cmake-compdb-gh-actions-sq/src/main.cpp" "/home/alvaro/repos/linux-cmake-compdb-gh-actions-sq/build/CMakeFiles/sonar_scanner_example.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
